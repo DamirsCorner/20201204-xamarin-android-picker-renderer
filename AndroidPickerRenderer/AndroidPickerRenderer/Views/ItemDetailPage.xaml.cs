bb7fdbb3f6a6e38d76e7f7fddf9897ee6b9fd7b1
Damir Arh <damir.arh@gmail.com>
2020-11-08 10:43:45 +0100

    Add project files.

---
+++ AndroidPickerRenderer/AndroidPickerRenderer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AndroidPickerRenderer.ViewModels;
+
+namespace AndroidPickerRenderer.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
