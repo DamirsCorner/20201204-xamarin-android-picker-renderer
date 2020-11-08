4d77df23b0609a3adce79cb910acce271aeb2489
Damir Arh <damir.arh@gmail.com>
2020-11-08 10:54:39 +0100

    Custom renderer with different popup

---
+++ AndroidPickerRenderer/AndroidPickerRenderer.Android/Renderers/BorderlessPickerRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidPickerRenderer.Controls;
+using AndroidPickerRenderer.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderlessPicker), typeof(BorderlessPickerRenderer))]
+namespace AndroidPickerRenderer.Droid.Renderers
+{
+    public class BorderlessPickerRenderer : PickerRenderer
+    {
+        public BorderlessPickerRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
---
+++ AndroidPickerRenderer/AndroidPickerRenderer.iOS/Renderers/BorderlessPickerRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidPickerRenderer.Controls;
+using AndroidPickerRenderer.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BorderlessPicker), typeof(BorderlessPickerRenderer))]
+namespace AndroidPickerRenderer.iOS.Renderers
+{
+    public class BorderlessPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+
+            if (Control == null)
+            {
+                return;
+            }
+
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
